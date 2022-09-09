afdfb2b7e032eddfc088c7146c2863ff1401f3ca
dbsalsgur <109213630+dbsalsgur@users.noreply.github.com>
2022-09-09 23:47:05 +0900

    220909 java 공부

---
+++ java_tutorials/src/org/java_study/javatutorials/eclipse/Helloworld.java
@@ -0,0 +1,9 @@
+package org.java_study.javatutorials.eclipse;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+       System.out.println("world Hello");
+	}
+
+} 
---
+++ java_tutorials/src/org/javastudy/java_tutorials/eclipse/Helloworld.java
@@ -0,0 +1,10 @@
+package org.javastudy.java_tutorials.eclipse;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
