67e1112362f3444cf00ca6677e7a72b91f8c22f9
dbsalsgur <109213630+dbsalsgur@users.noreply.github.com>
2022-09-10 12:15:25 +0900

    220910 java 공부

---
+++ java_tutorials/src/org/java_study/javatutorials/object/CalculatorDemo.java
@@ -0,0 +1,11 @@
+package org.java_study.javatutorials.object;
+
+public class CalculatorDemo {
+
+	public static void main(String[] args) {
+		// 아래의 로직이 복잡한 로직이라고 가정하자
+		System.out.println(10 + 20);
+        System.out.println(20 + 40);
+	}
+
+}
