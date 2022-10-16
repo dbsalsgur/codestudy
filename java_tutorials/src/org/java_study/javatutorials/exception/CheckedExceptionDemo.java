0821940b7d44bc82eb2e64e39fb26f306541cf3f
dbsalsgur <rkawkaos123@naver.com>
2022-10-16 22:56:08 +0900

    221016 java 공부

---
+++ java_tutorials/src/org/java_study/javatutorials/exception/CheckedExceptionDemo.java
@@ -0,0 +1,10 @@
+package org.java_study.javatutorials.exception;
+
+import java.io.*;
+public class CheckedExceptionDemo {
+    public static void main(String[] args) {
+        BufferedReader bReader = new BufferedReader(new FileReader("out.txt"));
+        String input = bReader.readLine();
+        System.out.println(input); 
+    }
+}
