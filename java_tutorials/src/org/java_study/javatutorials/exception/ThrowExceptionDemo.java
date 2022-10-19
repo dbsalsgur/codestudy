85b81f2afde0059afdd58acb90f83b56d3511abe
dbsalsgur <rkawkaos123@naver.com>
2022-10-19 23:17:53 +0900

    221019 java 공부

--- java_tutorials/src/org/java_study/javatutorials/exception/ThrowExceptionDemo.java
+++ java_tutorials/src/org/java_study/javatutorials/exception/ThrowExceptionDemo.java
@@ -1,18 +1,34 @@
 package org.java_study.javatutorials.exception;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 class B{
-    void run(){
+    void run() throws FileNotFoundException, IOException{
+    	BufferedReader bReader = null;
+        String input = null;
+		bReader = new BufferedReader(new FileReader("out.txt"));
+		input = bReader.readLine();
+        System.out.println(input); 
     }
 }
 class C{
-    void run(){
+    void run()throws FileNotFoundException, IOException{
         B b = new B();
-        b.run();
+			b.run();
     }
 }
 public class ThrowExceptionDemo {
     public static void main(String[] args) {
          C c = new C();
-         c.run();
+         try {
+			c.run();
+		} catch (FileNotFoundException e) {
+			System.out.println("out.txt 파일이 필요합니다");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }   
 }
