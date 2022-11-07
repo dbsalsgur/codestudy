ff037f6477c697811702742522c95b56f1b0a735
dbsalsgur <rkawkaos123@naver.com>
2022-11-07 22:56:15 +0900

    221107 java 공부

--- java_tutorials/src/org/java_study/javatutorials/constant2/ConstantDemo.java
+++ java_tutorials/src/org/java_study/javatutorials/constant2/ConstantDemo.java
@@ -1,33 +1,36 @@
 package org.java_study.javatutorials.constant2;
 
-class Fruit{
-    public static final Fruit APPLE  = new Fruit();
-    public static final Fruit PEACH  = new Fruit();
-    public static final Fruit BANANA = new Fruit();
+//class Fruit{
+//	public static final Fruit APPLE = new Fruit();
+//	public static final Fruit PEACH = new Fruit();
+//	public static final Fruit BANANA = new Fruit();
+//}
+//class Company{
+//	public static final Fruit GOOGLE = new Company();
+//	public static final Fruit APPLE = new Company();
+//	public static final Fruit ORACLE = new Company();
+//}
+enum Fruit{
+	APPLE, PEACH, BANANA;
 }
-class Company{
-    public static final Company GOOGLE = new Company();
-    public static final Company APPLE = new Company();
-    public static final Company ORACLE = new Company();
+enum Company{
+	GOOGLE, APPLE, ORACLE;
 }
  
 public class ConstantDemo {
      
     public static void main(String[] args) {
-//        if(Fruit.APPLE == Company.APPLE){
-//            System.out.println("과일 애플과 회사 애플이 같다.");
-//        }
-//        Fruit type = Fruit.APPLE;
-//        switch(type){
-//            case Fruit.APPLE:
-//                System.out.println(57+" kcal");
-//                break;
-//            case Fruit.PEACH:
-//                System.out.println(34+" kcal");
-//                break;
-//            case Fruit.BANANA:
-//                System.out.println(93+" kcal");
-//                break;
-//        }
+        Fruit type = Fruit.APPLE;
+        switch(type){
+            case APPLE:
+                System.out.println(57+" kcal");
+                break;
+            case PEACH:
+                System.out.println(34+" kcal");
+                break;
+            case BANANA:
+                System.out.println(93+" kcal");
+                break;
+        }
     }
 }
