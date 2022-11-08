c429a3c47c4f152d96115cb1176c2d64a62b5700
dbsalsgur <rkawkaos123@naver.com>
2022-11-08 22:38:59 +0900

    221108 java 공부

--- java_tutorials/src/org/java_study/javatutorials/constant2/ConstantDemo.java
+++ java_tutorials/src/org/java_study/javatutorials/constant2/ConstantDemo.java
@@ -11,7 +11,15 @@ package org.java_study.javatutorials.constant2;
 //	public static final Fruit ORACLE = new Company();
 //}
 enum Fruit{
-	APPLE, PEACH, BANANA;
+	APPLE("red"), PEACH("pink"), BANANA("yellow");
+	private String color;
+	public String getColor(){
+		return this.color;
+	}
+	Fruit(String color){
+		System.out.println("Call Constructor "+this);
+		this.color = color;
+	}
 }
 enum Company{
 	GOOGLE, APPLE, ORACLE;
@@ -20,16 +28,19 @@ enum Company{
 public class ConstantDemo {
      
     public static void main(String[] args) {
+    	for(Fruit f : Fruit.values()){
+    		System.out.println(f+", "+f.getColor());
+    	}
         Fruit type = Fruit.APPLE;
         switch(type){
             case APPLE:
-                System.out.println(57+" kcal");
+                System.out.println(57+" kcal color : "+Fruit.APPLE.getColor());
                 break;
             case PEACH:
-                System.out.println(34+" kcal");
+                System.out.println(34+" kcal color : "+Fruit.PEACH.getColor());
                 break;
             case BANANA:
-                System.out.println(93+" kcal");
+                System.out.println(93+" kcal color : "+Fruit.BANANA.getColor());
                 break;
         }
     }
