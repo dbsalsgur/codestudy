695cb37c242e106f70fe43badd47e19072957064
dbsalsgur <rkawkaos123@naver.com>
2022-11-11 22:56:15 +0900

    221111 java 공부

--- java_tutorials/src/org/java_study/javatutorials/reference/ReferenceDemo1.java
+++ java_tutorials/src/org/java_study/javatutorials/reference/ReferenceDemo1.java
@@ -1,5 +1,10 @@
 package org.java_study.javatutorials.reference;
-
+class A{
+	public int id;
+	A(int id){
+		this.id = id;
+	}
+}
 public class ReferenceDemo1 {
 	 
     public static void runValue(){
@@ -7,10 +12,20 @@ public class ReferenceDemo1 {
         int b = a;
         b = 2;
         System.out.println("runValue, "+a); 
+        //결과 runValue, 1
+    }
+ 
+    public static void runReference(){
+        A a = new A(1);
+        A b = a;
+        b.id = 2;
+        System.out.println("runReference, "+a.id);      
+        //결과 runReference, 2
     }
  
     public static void main(String[] args) {
         runValue();
+        runReference();
     }
  
 }
