c119a8c38892fdb4227b1728e1500adf039890c5
dbsalsgur <rkawkaos123@naver.com>
2022-12-10 22:05:18 +0900

    221210 java 공부

---
+++ java_tutorials/src/org/java_study/javatutorials/collection/MapDemo.java
@@ -0,0 +1,38 @@
+package org.java_study.javatutorials.collection;
+
+import java.util.*;
+
+public class MapDemo {
+ 
+    public static void main(String[] args) {
+        HashMap<String, Integer> a = new HashMap<String, Integer>();
+        a.put("one", 1);
+        a.put("two", 2);
+        a.put("three", 3);
+        a.put("four", 4);
+        System.out.println(a.get("one"));
+        System.out.println(a.get("two"));
+        System.out.println(a.get("three"));
+         
+        iteratorUsingForEach(a);
+        iteratorUsingIterator(a);
+    }
+     
+    static void iteratorUsingForEach(HashMap map){
+        Set<Map.Entry<String, Integer>> entries = map.entrySet();
+        //getKey()의 값이 String, getValue()의 값이 Integer라는 형식을 가진다.
+        for (Map.Entry<String, Integer> entry : entries) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+     
+    static void iteratorUsingIterator(HashMap map){
+        Set<Map.Entry<String, Integer>> entries = map.entrySet();
+        Iterator<Map.Entry<String, Integer>> i = entries.iterator();
+        while(i.hasNext()){
+            Map.Entry<String, Integer> entry = i.next();
+            System.out.println(entry.getKey()+" : "+entry.getValue());
+        }
+    }
+ 
+}
