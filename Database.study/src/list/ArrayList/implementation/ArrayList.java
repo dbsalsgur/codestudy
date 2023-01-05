4ea2651e713f121964d10372109873d5b5eebe07
dbsalsgur <rkawkaos123@naver.com>
2023-01-05 23:41:22 +0900

    230105 DataStructure 공부

---
+++ Database.study/src/list/ArrayList/implementation/ArrayList.java
@@ -0,0 +1,12 @@
+package list.ArrayList.implementation;
+
+public class ArrayList {
+	private int size = 0;
+	private Object[] elementData = new Object[100];
+	
+	public boolean addLast(Object element) {
+		elementData[size] = element;
+		size++;
+		return true;
+	}
+}
