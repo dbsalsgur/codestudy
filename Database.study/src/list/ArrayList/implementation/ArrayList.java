09ee066e2bc07d656ac0c4b16ee07267b52d19c3
dbsalsgur <rkawkaos123@naver.com>
2023-01-08 23:20:32 +0900

    230108 DataStructure 공부

--- Database.study/src/list/ArrayList/implementation/ArrayList.java
+++ Database.study/src/list/ArrayList/implementation/ArrayList.java
@@ -33,4 +33,41 @@ public class ArrayList {
 		}
 		return str + "]";
 	}
+
+	public Object remove(int index) {
+		Object removed = elementData[index];
+		for (int i = index+1; i <= size-1; i++) {
+			elementData[i-1] = elementData[i];
+		}
+		size--;
+		elementData[size] = null;
+		return removed;
+		//원래 메소드를 void로 설정하고 리턴값이 없이 해도 동작은 잘 한다. 
+		//하지만 Collection의 remove에서는 삭제된 값을 리턴하기 때문에 그것까지 구현하도록 한다.
+	}
+	
+	public Object removeFirst() {
+			return remove(0);
+	}
+	
+	public Object removeLast() {
+		return remove(size-1);
+	}
+	
+	public Object get(int index) {
+		return elementData[index];
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public int indexOf(Object o) {
+		for (int i = 0; i < size; i++) {
+			if (o.equals(elementData[i])) {
+				return i;
+			}
+		}
+		return -1;
+	}
 }
--- Database.study/src/list/ArrayList/implementation/Main.java
+++ Database.study/src/list/ArrayList/implementation/Main.java
@@ -8,9 +8,17 @@ public class Main {
 		numbers.addLast(20);
 		numbers.addLast(30);
 		numbers.addLast(40);
+		System.out.println(numbers);
+		System.out.println(numbers.get(1));
+		System.out.println(numbers.size());
+		System.out.println(numbers.indexOf(70));
 		numbers.add(1, 15);
 		numbers.addFirst(5);
 		System.out.println(numbers);
+		System.out.println(numbers.remove(1));
+		numbers.removeFirst();
+		System.out.println(numbers);
+		
 	}
 
 }
